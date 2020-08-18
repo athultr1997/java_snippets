935bfdc404ddf33cf54319378011309f57fe9824
athultr1997 <athultr15@gmail.com>
2020-08-18 07:15:58 -0700

    folder structure changed

--- 02_classes_intro/src/main/java/class_intro/Animals.java
+++
@@ -1,9 +0,0 @@
-package classes_intro;
-
-public abstract class Animals {
-	protected Integer age;
-
-	public abstract void setAge(Integer age);
-
-	public abstract Integer getAge();
-}
--- 02_classes_intro/src/main/java/class_intro/README.md
+++
@@ -1,5 +0,0 @@
-# How to Compile ?
->> mvn compile
-
-# How to run tests?
->> mvn test
